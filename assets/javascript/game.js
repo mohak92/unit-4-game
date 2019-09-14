8ff854d0907492258a24ecbf5d430b0a31075c73
mohak92 <tamhane.mohak@gmail.com>
2019-09-14 18:01:52 -0400

    UI implemented final touches to HTML page additional styles added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,22 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var Character = function(healthPoints, attackPower, counterAttackPower){
-        this.healthPoints = healthPoints;
-        this.attackPower = attackPower;
-        this.counterAttackPower = counterAttackPower;
+    class Character {
+        constructor(name, healthPoints, attackPower, counterAttackPower) {
+            this.name = name;
+            this.healthPoints = healthPoints;
+            this.attackPower = attackPower;
+            this.counterAttackPower = counterAttackPower;
+        }
     }
 
-    //console.log(player1);
+    player1 = new Character(120, incrementAttack(6), 20);
+
+    player2 = new Character(120, incrementAttack(6), 20);
+
+    player3 = new Character(120, incrementAttack(6), 20);
+
+    player4 = new Character(120, incrementAttack(6), 20);
 
     function incrementAttack(attackPower){
         return attackPower += attackPower;
